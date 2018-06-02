1e565ee1710d0567665f62028b8eb607def40e2c
mbichinsky <maria.bichinsky@gmail.com>
2018-06-02 21:00:54 +0000

    adding 2 more book projects

--- F-to-C.c
+++ F-to-C.c
@@ -1,5 +1,19 @@
-#include stdio.h
+#include <stdio.h>
 /*this program prints the conversion table of F to C*/
-
+/*this will print F to C table starting at 0F increasing by 10*/
 main()
 {
+  float fahr, celsius;
+  float lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 10;
+
+  fahr = lower;
+  while (fahr <= upper){
+    celsius = 5 * (fahr-32) / 9;
+    printf ("%3.0f%6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
